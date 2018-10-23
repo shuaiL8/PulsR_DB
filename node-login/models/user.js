3c7b964fd60e807251a129fba543afc37a34f6bd
fred <fred-liu@outlook.com>
2018-10-22 21:49:58 -0400

    aws

--- node-login/models/user.js
+++ node-login/models/user.js
@@ -4,18 +4,18 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const userSchema = mongoose.Schema({ 
+const userSchema = mongoose.Schema({
 
 	name 			: String,
-	email			: {type: String, unique: true}, 
+	email			: {type: String, unique: true},
 	hashed_password	: String,
 	created_at		: String,
 	temp_password	: String,
 	temp_password_time: String
-	
+
 });
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/node-login');
+mongoose.connect('mongodb://54.157.40.239:27017/node-login');
 
-module.exports = mongoose.model('user', userSchema);        
+module.exports = mongoose.model('user', userSchema);
